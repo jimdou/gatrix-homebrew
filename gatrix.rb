75c7599931a8b7d51e90d5e1aad7bbf2347b20c0
jimdou <jimmydouieb@gmail.com>
2018-03-06 15:17:21 +0200

    Test

--- gatrix.rb
+++ gatrix.rb
@@ -1,11 +1,9 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
- class Gatrix < Formula
-  desc ""
-  homepage ""
+class Gatrix < Formula
+  desc "The Git Matrix"
+  homepage "https://gatrix.io"
   url "https://github.com/jimdou/gatrix/archive/v1.1.0.tar.gz"
-  sha256 "eeffc7f64280ca3a79ca9fad9409b4d8784e9cc0aa51813d7b0b08b62953f18b"
+  version "v1.1.0"
+  sha256 ""
 
   def install
     bin.install "gatrix"
