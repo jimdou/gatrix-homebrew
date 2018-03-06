8cf75cfaf66bb3c8b172c0e310586c43bdba7e79
jimdou <jimmydouieb@gmail.com>
2018-03-06 15:33:09 +0200

    v1.2.0

--- gatrix.rb
+++ gatrix.rb
@@ -1,8 +1,8 @@
 class Gatrix < Formula
-  desc "The Git Matrix"
+  desc "Gatrix"
   homepage "https://gatrix.io"
-  url "https://github.com/jimdou/gatrix/archive/v1.1.0.tar.gz"
-  version "v1.1.0"
+  url "https://github.com/jimdou/gatrix/archive/v1.2.0.tar.gz"
+  version "v1.2.0"
   sha256 "76e9451945203f130513ed9e21a3a66eb7d3c39304a35b7842d529dfa33ddaca"
   def install
     bin.install "gatrix"
