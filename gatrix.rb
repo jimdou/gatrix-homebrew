e39f1b72ae60daeb077ce31c36a1c48b70d8df77
jimdou <jimmydouieb@gmail.com>
2018-03-06 15:26:17 +0200

    Add SSH

--- gatrix.rb
+++ gatrix.rb
@@ -3,10 +3,8 @@ class Gatrix < Formula
   homepage "https://gatrix.io"
   url "https://github.com/jimdou/gatrix/archive/v1.1.0.tar.gz"
   version "v1.1.0"
-  sha256 ""
-
+  sha256 "76e9451945203f130513ed9e21a3a66eb7d3c39304a35b7842d529dfa33ddaca"
   def install
     bin.install "gatrix"
   end
-
 end
