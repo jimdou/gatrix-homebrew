49efddc55ef0846dfe5f992f0244bd80dde25679
jimdou <jimmydouieb@gmail.com>
2018-03-06 20:52:42 +0200

    Fix

--- gatrix.rb
+++ gatrix.rb
@@ -2,7 +2,7 @@ class Gatrix < Formula
   desc "Gatrix"
   homepage "https://gatrix.io"
   url "https://github.com/jimdou/gatrix/archive/v1.3.0.tar.gz"
-  version "v1.3.0"
+  version "1.3.0"
   sha256 ""
   def install
     bin.install "gatrix"
