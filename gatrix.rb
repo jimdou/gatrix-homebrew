69407d986ba0c170ae24cc4155436e693b6fa7c8
jimdou <jimmydouieb@gmail.com>
2018-03-09 20:22:11 +0200

    checksum

--- gatrix.rb
+++ gatrix.rb
@@ -3,7 +3,7 @@ class Gatrix < Formula
   homepage "https://gatrix.io"
   url "https://github.com/jimdou/gatrix/archive/v1.14.0.tar.gz"
   version "1.14.0"
-  sha256 ""
+  sha256 "d4d3b8100b8bd5bdc332534ad0fa7cb320619566d6fc4640b16c48cfecb67fa5"
   def install
     bin.install "gatrix"
   end
